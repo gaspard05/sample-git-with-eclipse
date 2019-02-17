aad4b3c64feaf340e3c7d202b4335ec9dcc301a1
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-17 21:58:36 +0000

    Added age field

--- src/com/indevopslab/samplegit/model/User.java
+++ src/com/indevopslab/samplegit/model/User.java
@@ -3,6 +3,7 @@ package com.indevopslab.samplegit.model;
 public class User {
     
     private String name;
+    private int age;
 
     public String getName() {
         return name;
@@ -11,6 +12,13 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-    
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
 }

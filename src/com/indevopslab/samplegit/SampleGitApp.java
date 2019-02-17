468427e8f9d6e847f615d05330ea6a473aa45c38
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-17 00:51:30 +0000

    SamplaGitApp class created

---
+++ src/com/indevopslab/samplegit/SampleGitApp.java
@@ -0,0 +1,11 @@
+package com.indevopslab.samplegit;
+
+public class SampleGitApp {
+
+    public static void main(String[] args) {
+        System.out.println("====>Starting");
+      System.out.println("Hello World, from InDevOpLab");
+
+    }
+
+}

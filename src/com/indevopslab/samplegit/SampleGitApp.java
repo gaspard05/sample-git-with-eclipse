0f0872444e8e445aedd3c145fb2dd7e23c502d88
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-17 00:53:32 +0000

    Typo corrected in InDevOpsLab

--- src/com/indevopslab/samplegit/SampleGitApp.java
+++ src/com/indevopslab/samplegit/SampleGitApp.java
@@ -4,7 +4,7 @@ public class SampleGitApp {
 
     public static void main(String[] args) {
         System.out.println("====>Starting");
-      System.out.println("Hello World, from InDevOpLab");
+      System.out.println("Hello World, from InDevOpsLab");
       System.out.println("I am loving it!");
 
     }

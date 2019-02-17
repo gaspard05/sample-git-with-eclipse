4de6cefe896f1b58d6d45d232d104089e4ed0a99
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-17 00:54:41 +0000

    Text alignment!

--- src/com/indevopslab/samplegit/SampleGitApp.java
+++ src/com/indevopslab/samplegit/SampleGitApp.java
@@ -3,7 +3,7 @@ package com.indevopslab.samplegit;
 public class SampleGitApp {
 
     public static void main(String[] args) {
-        System.out.println("====>Starting");
+      System.out.println("====>Starting");
       System.out.println("Hello World, from InDevOpsLab");
       System.out.println("I am loving it!");
 

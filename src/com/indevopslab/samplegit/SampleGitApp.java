89faf469e6411b312858d5a446c4ebdabcf6373f
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-17 21:17:17 +0000

    Using User model in main method

--- src/com/indevopslab/samplegit/SampleGitApp.java
+++ src/com/indevopslab/samplegit/SampleGitApp.java
@@ -1,12 +1,17 @@
 package com.indevopslab.samplegit;
 
+import com.indevopslab.samplegit.model.User;
+
 public class SampleGitApp {
+    
+    private static User user = new User();
 
     public static void main(String[] args) {
+        
       System.out.println("====>Starting");
+      user.setName("Gaspard");
       System.out.println("Hello World, from InDevOpsLab");
-      System.out.println("I am loving it!");
+      System.out.println( user.getName() + " is loving it! " );
 
     }
-
 }
